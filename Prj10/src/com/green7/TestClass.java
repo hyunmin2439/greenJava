2622c3bfe874997755dff4f859b2458f45a24f18
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-06-11 17:45:19 +0900

    Delete Prj10 directory

--- Prj10/src/com/green7/TestClass.java
+++
@@ -1,9 +0,0 @@
-package com.green7;
-
-// public class TestClass extends Object extends ParentClass
-// public class TestClass extends Object, ParentClass 
-// 위의 두문장은 둘다 불가능. java, C#은 단일 상속. C++은 다중상속 가능
-// Object는 생략되어 자동 상속이 되도록 만들어놓았다.
-public class TestClass {
-
-}
