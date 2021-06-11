392bfac4a3ddbebb6770030c011d869281001e79
hyunmin2439 <72420501+hyunmin2439@users.noreply.github.com>
2021-06-11 17:48:50 +0900

    Add files via upload

---
+++ Prj12/src/com/green/Raccoon.java
@@ -0,0 +1,13 @@
+package com.green;
+
+public class Raccoon {
+	String name;
+	
+	void nuggle() {
+		System.out.println(name + "(이)가 너굴너굴");
+	}
+	
+	void dig() {
+		System.out.println(name + "(이)가 땅을 판다");
+	}
+}
